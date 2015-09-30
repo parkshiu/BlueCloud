39c642b13a0ea2604e4044e4be76d8ec810238a5
P_S2oL <parkshiu@naver.com>
2015-09-30 13:42:19 +0900

    20150922
    python

---
+++ age.py
@@ -0,0 +1,41 @@
+print "당신이 태어난 년도를 입력하세요"
+year=int(raw_input())
+
+age=2015-year+1
+
+if 20<=age<=26:
+    print"대학생"
+elif 17<=age<20:
+    print"고등학생"
+elif 14<=age<17:
+    print"중학생"
+elif 8<=age<15:
+    print"초등학생"
+else:
+    print"학생이 아니다."
+
+
+
+for i in 'abcdefg':
+    print i
+
+for i in range(1,10,2):
+    print i
+
+for i in range(10,1,-1):
+    print i
+
+
+for i in ["song","sang","jun"]:
+    print i
+
+
+print "구구단 몇단을 계산할까요?"
+n=int(raw_input())
+
+print "구구단"n"단계산합니다."
+for i in range(1,9):
+    print n*i=
+    
+    
+    
