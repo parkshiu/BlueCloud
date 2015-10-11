b5e8b81eec707f1f3904d3bfa908633468823ab9
P_S2oL <parkshiu@naver.com>
2015-10-11 17:35:11 +0900

    20151005 python
    20151005 python

---
+++ 1005.py
@@ -0,0 +1,17 @@
+i = 1
+while i < 10:
+    print i
+    i += 1
+
+
+for i in range(10):
+    if i == 5: break
+    print i
+print "EOP"
+
+
+for i in range(10):
+    print i,
+else:
+    print"EOF"
+
